7eba2b88286b72b94364dad6afc1507c83b73163
jnillo <jnillo9@gmail.com>
2012-08-11 08:51:59 +0200

    Add code comments

--- javascripts/functions.js
+++ javascripts/functions.js
@@ -1,18 +1,30 @@
 // Global Variables 
-var news_per_pages = 2; // News per pages
-var xml_news = "";
 
-// Functions:
+var news_per_pages = 2; // News per pages
+var xml_news = "";      
+
+/*
+  Extra functions 
+*/
+
+// This function load a partial from a link. Used in navigation menu
 
 function load_page(page,id){
    $('#'+id).load(page);
 }
 
+
+// Function to load news from xml: notices/news.xml . 
+// Initial call load the last <news_per_pages> news from xml file.
+
 function load_news(page,xml){
  
+   // Load xml file value in a global variable
    if(xml_news==""){
     xml_news = xml;
    }
+
+   // Default values for xml and page variables: 
    xml = typeof xml !== "undefined" ? xml : xml_news;
    page = typeof page !== "undefined" ? page : 1;
 
@@ -27,17 +39,23 @@ function load_news(page,xml){
 
    news = all_news.slice(start,end);
 
+   // Build news blocks.
    news.each(function(){
       html = html+"<div class='row'><div class='twelve columns'>";
       html = html + "<h3>"+$("title",this).text()+"</h3>";
       html = html + unescape($("body",this).text()) +"";
       html = html + '</div></div>';
    });   
+
+   // Build pagination menu
    if(count > news_per_pages){
       html = html + news_paginations(all_news,page);   
    }
+
    $('#center').html(html);
 }
+
+// Function to build pagination menu
 
 function news_paginations(news,page)
 {
@@ -46,14 +64,17 @@ function news_paginations(news,page)
      count = 1 ;
  
  for(i=0; i < (news.length % news_per_pages) +1; i++){
+    // If is current page...
     if(page != count){
       html = html + '<li>';
     }else{  
       html = html + '<li class="current">';
     }
+    
     html = html + '<a href="#" onclick="javascript:load_news('+count+');">'+count+'</a></li>';
     count++;
  }
+
  //html = html + "<li class='arrow'><a href="" onclick='javascript:load_new(xml_news,-2);'>&raquo;</a></li>";
 
  html = html + "</ul></div></div>";
